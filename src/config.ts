0c2512ced5fba1c46c7d561f5ca7abab74c797fb
EnaShepherd <akaidenki@gmail.com>
2025-09-18 20:07:27 +0200

    change website description

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Home",
-	subtitle: "Demo Site",
+	title: "Ena's blog",
+	subtitle: "my personal projects and interesting stuff i do",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
